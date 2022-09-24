0c55e6ba8d96c7aa2cc48ff4a04b05cdaff38129
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-09-24 18:03:06 -0300

    Service

--- src/main/java/com/ecommerce/api/app/service/ClienteService.java
+++ src/main/java/com/ecommerce/api/app/service/ClienteService.java
@@ -3,16 +3,22 @@ package com.ecommerce.api.app.service;
 import java.util.List;
 
 import com.ecommerce.api.app.model.Cliente;
-import com.ecommerce.api.app.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ClienteService extends ClienteRepository{
+public class ClienteService {
+	public void guardarCliente(Cliente cliente) {
+	}
 
-	public void guardarCliente(Cliente cliente);
-	
-	public List<Cliente> obtenerClientes();
-	
-	public Cliente obtenerUnCliente(Integer id);
-	
+	public List<Cliente> obtenerClientes() {
+
+		return null;
+	}
+
+	public Cliente obtenerUnCliente(Integer id){
+		return null;
+	}
+
+
+
 }
