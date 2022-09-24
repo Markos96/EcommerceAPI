e926227b4fb62bafaff244cfae1baf3bf6b64798
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-09-24 17:56:38 -0300

    modificando paquetes 2

--- src/main/java/com/ecommerce/api/app/repository/ClienteRepository.java
+++ src/main/java/com/ecommerce/api/app/repository/ClienteRepository.java
@@ -3,7 +3,9 @@ package com.ecommerce.api.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.api.app.model.Cliente;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
--- src/main/java/com/ecommerce/api/app/service/ClienteService.java
+++ src/main/java/com/ecommerce/api/app/service/ClienteService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.ecommerce.api.app.model.Cliente;
 import com.ecommerce.api.app.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ClienteService extends ClienteRepository{
 
 	public void guardarCliente(Cliente cliente);
